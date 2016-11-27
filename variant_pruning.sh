d99830fcfec54f6318daed608f4c84b865cf5979
Pyry Helkkula <pyry.helkkula@gmail.com>
2016-11-27 20:23:11 +0200

    VCF import

--- variant_pruning.sh
+++ variant_pruning.sh
@@ -2,4 +2,8 @@
 
 PLINK="path_to_plink"
 HIGHLD="high-LD-regions.txt"
-VCF="ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz"
+VCF="ALL.chr20.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
+NTHREADS=4
+
+#Convert VCF into bfiles
+$PLINK --vcf $VCF --threads $NTHREADS --make-bed --out input

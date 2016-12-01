42f05190ad232da78f4bd9caff98c538ca3bd7c0
Pyry Helkkula <pyry.helkkula@gmail.com>
2016-12-01 12:56:56 +0200

    Started plot_manifolds.py

---
+++ plot_manifolds.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS, TSNE, SpectralEmbedding
+import matplotlib.cm as cm 
+import matplotlib.pyplot as plt 

958764e9b86a05679c1967240303dac05ba3d8e2
Pyry Helkkula <pyry.helkkula@gmail.com>
2016-12-01 13:18:51 +0200

    Read in allelic dosages

--- plot_manifolds.py
+++ plot_manifolds.py
@@ -5,4 +5,7 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.manifold import MDS, TSNE, SpectralEmbedding
 import matplotlib.cm as cm 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
+# Read in allelic dosages
+dosage_df = pd.read_csv('pruned.raw', sep=' ')
